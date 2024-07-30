843d0f96e48f00fc18867a6c2e1eff465fd2d164
Luden <pavelluden@gmail.com>
2024-07-31 01:49:34 +0300

    GTM-0 install.cmd

---
+++ install.cmd
@@ -0,0 +1,5 @@
+call "%~dp0argos/install_argos.cmd"
+call "%~dp0whisperx/install_whisperx.cmd"
+call "%~dp0subtitle_maker/install_sub_maker.cmd"
+echo Installation complete
+pause
---
+++ subtitle_maker/install_sub_maker.cmd
@@ -0,0 +1,3 @@
+call "%~dp0activate_env"
+python -m pip install --upgrade pip
+pip install openai
