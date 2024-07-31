4cbc0b91c4a07be9f750e88d09023817d97f794d
Luden <pavelluden@gmail.com>
2024-07-31 23:08:37 +0300

    GTM-0 wip installation

--- subtitle_maker/install_sub_maker.cmd
+++ subtitle_maker/install_sub_maker.cmd
@@ -1,5 +1,5 @@
 call "%~dp0activate_env"
 python -m pip install --upgrade pip
 pip install openai
-python configuration.py
-start "" "config.json"
+python "%~dp0configuration.py"
+start "" "%~dp0config.json"
--- whisperx/install_whisperx.cmd
+++ whisperx/install_whisperx.cmd
@@ -1,4 +1,5 @@
 call "%~dp0activate_env"
 python -m pip install --upgrade pip
 pip install torch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 --index-url https://download.pytorch.org/whl/cu121
-pip install git+https://github.com/m-bain/whisperx.git
+pip install ffmpeg
+pip install whisperx
